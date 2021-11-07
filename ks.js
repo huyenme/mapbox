27f0c29b548a0b904dfcf7d3bcae486446845281
huyenme <42825021+huyenme@users.noreply.github.com>
2021-11-07 15:48:16 -0600

    Create ks.js

---
+++ ks.js
@@ -0,0 +1,525 @@
+[{
+        "code": "us-ks-001",
+        "name": "Allen County, KS",
+        "value": 4.4
+    },
+    {
+        "code": "us-ks-003",
+        "name": "Anderson County, KS",
+        "value": 4.3
+    },
+    {
+        "code": "us-ks-005",
+        "name": "Atchison County, KS",
+        "value": 5.5
+    },
+    {
+        "code": "us-ks-007",
+        "name": "Barber County, KS",
+        "value": 2.6
+    },
+    {
+        "code": "us-ks-009",
+        "name": "Barton County, KS",
+        "value": 3.8
+    },
+    {
+        "code": "us-ks-011",
+        "name": "Bourbon County, KS",
+        "value": 5.1
+    },
+    {
+        "code": "us-ks-013",
+        "name": "Brown County, KS",
+        "value": 3.4
+    },
+    {
+        "code": "us-ks-015",
+        "name": "Butler County, KS",
+        "value": 3.8
+    },
+    {
+        "code": "us-ks-017",
+        "name": "Chase County, KS",
+        "value": 3.5
+    },
+    {
+        "code": "us-ks-019",
+        "name": "Chautauqua County, KS",
+        "value": 4.5
+    },
+    {
+        "code": "us-ks-021",
+        "name": "Cherokee County, KS",
+        "value": 4.1
+    },
+    {
+        "code": "us-ks-023",
+        "name": "Cheyenne County, KS",
+        "value": 2.2
+    },
+    {
+        "code": "us-ks-025",
+        "name": "Clark County, KS",
+        "value": 2.9
+    },
+    {
+        "code": "us-ks-027",
+        "name": "Clay County, KS",
+        "value": 4
+    },
+    {
+        "code": "us-ks-029",
+        "name": "Cloud County, KS",
+        "value": 3.7
+    },
+    {
+        "code": "us-ks-031",
+        "name": "Coffey County, KS",
+        "value": 5.5
+    },
+    {
+        "code": "us-ks-033",
+        "name": "Comanche County, KS",
+        "value": 2.8
+    },
+    {
+        "code": "us-ks-035",
+        "name": "Cowley County, KS",
+        "value": 3.5
+    },
+    {
+        "code": "us-ks-037",
+        "name": "Crawford County, KS",
+        "value": 4.5
+    },
+    {
+        "code": "us-ks-039",
+        "name": "Decatur County, KS",
+        "value": 2.6
+    },
+    {
+        "code": "us-ks-041",
+        "name": "Dickinson County, KS",
+        "value": 4.1
+    },
+    {
+        "code": "us-ks-043",
+        "name": "Doniphan County, KS",
+        "value": 3.8
+    },
+    {
+        "code": "us-ks-045",
+        "name": "Douglas County, KS",
+        "value": 3.2
+    },
+    {
+        "code": "us-ks-047",
+        "name": "Edwards County, KS",
+        "value": 2.7
+    },
+    {
+        "code": "us-ks-049",
+        "name": "Elk County, KS",
+        "value": 3.4
+    },
+    {
+        "code": "us-ks-051",
+        "name": "Ellis County, KS",
+        "value": 2.5
+    },
+    {
+        "code": "us-ks-053",
+        "name": "Ellsworth County, KS",
+        "value": 2.9
+    },
+    {
+        "code": "us-ks-055",
+        "name": "Finney County, KS",
+        "value": 2.4
+    },
+    {
+        "code": "us-ks-057",
+        "name": "Ford County, KS",
+        "value": 2.5
+    },
+    {
+        "code": "us-ks-059",
+        "name": "Franklin County, KS",
+        "value": 4.5
+    },
+    {
+        "code": "us-ks-061",
+        "name": "Geary County, KS",
+        "value": 4.8
+    },
+    {
+        "code": "us-ks-063",
+        "name": "Gove County, KS",
+        "value": 2.4
+    },
+    {
+        "code": "us-ks-065",
+        "name": "Graham County, KS",
+        "value": 3.1
+    },
+    {
+        "code": "us-ks-067",
+        "name": "Grant County, KS",
+        "value": 2.9
+    },
+    {
+        "code": "us-ks-069",
+        "name": "Gray County, KS",
+        "value": 1.9
+    },
+    {
+        "code": "us-ks-071",
+        "name": "Greeley County, KS",
+        "value": 2.1
+    },
+    {
+        "code": "us-ks-073",
+        "name": "Greenwood County, KS",
+        "value": 4.3
+    },
+    {
+        "code": "us-ks-075",
+        "name": "Hamilton County, KS",
+        "value": 2
+    },
+    {
+        "code": "us-ks-077",
+        "name": "Harper County, KS",
+        "value": 3.1
+    },
+    {
+        "code": "us-ks-079",
+        "name": "Harvey County, KS",
+        "value": 3.6
+    },
+    {
+        "code": "us-ks-081",
+        "name": "Haskell County, KS",
+        "value": 2.2
+    },
+    {
+        "code": "us-ks-083",
+        "name": "Hodgeman County, KS",
+        "value": 2.5
+    },
+    {
+        "code": "us-ks-085",
+        "name": "Jackson County, KS",
+        "value": 3.6
+    },
+    {
+        "code": "us-ks-087",
+        "name": "Jefferson County, KS",
+        "value": 4.2
+    },
+    {
+        "code": "us-ks-089",
+        "name": "Jewell County, KS",
+        "value": 2.7
+    },
+    {
+        "code": "us-ks-091",
+        "name": "Johnson County, KS",
+        "value": 3.1
+    },
+    {
+        "code": "us-ks-093",
+        "name": "Kearny County, KS",
+        "value": 2.2
+    },
+    {
+        "code": "us-ks-095",
+        "name": "Kingman County, KS",
+        "value": 3.4
+    },
+    {
+        "code": "us-ks-097",
+        "name": "Kiowa County, KS",
+        "value": 2.2
+    },
+    {
+        "code": "us-ks-099",
+        "name": "Labette County, KS",
+        "value": 4.1
+    },
+    {
+        "code": "us-ks-101",
+        "name": "Lane County, KS",
+        "value": 2.7
+    },
+    {
+        "code": "us-ks-103",
+        "name": "Leavenworth County, KS",
+        "value": 4.6
+    },
+    {
+        "code": "us-ks-105",
+        "name": "Lincoln County, KS",
+        "value": 3
+    },
+    {
+        "code": "us-ks-107",
+        "name": "Linn County, KS",
+        "value": 7.1
+    },
+    {
+        "code": "us-ks-109",
+        "name": "Logan County, KS",
+        "value": 2.4
+    },
+    {
+        "code": "us-ks-111",
+        "name": "Lyon County, KS",
+        "value": 4
+    },
+    {
+        "code": "us-ks-113",
+        "name": "McPherson County, KS",
+        "value": 2.5
+    },
+    {
+        "code": "us-ks-115",
+        "name": "Marion County, KS",
+        "value": 3.3
+    },
+    {
+        "code": "us-ks-117",
+        "name": "Marshall County, KS",
+        "value": 3.1
+    },
+    {
+        "code": "us-ks-119",
+        "name": "Meade County, KS",
+        "value": 2.2
+    },
+    {
+        "code": "us-ks-121",
+        "name": "Miami County, KS",
+        "value": 4.5
+    },
+    {
+        "code": "us-ks-123",
+        "name": "Mitchell County, KS",
+        "value": 2.2
+    },
+    {
+        "code": "us-ks-125",
+        "name": "Montgomery County, KS",
+        "value": 4.7
+    },
+    {
+        "code": "us-ks-127",
+        "name": "Morris County, KS",
+        "value": 3.2
+    },
+    {
+        "code": "us-ks-129",
+        "name": "Morton County, KS",
+        "value": 3
+    },
+    {
+        "code": "us-ks-131",
+        "name": "Nemaha County, KS",
+        "value": 2.5
+    },
+    {
+        "code": "us-ks-133",
+        "name": "Neosho County, KS",
+        "value": 5
+    },
+    {
+        "code": "us-ks-135",
+        "name": "Ness County, KS",
+        "value": 2.4
+    },
+    {
+        "code": "us-ks-137",
+        "name": "Norton County, KS",
+        "value": 2.1
+    },
+    {
+        "code": "us-ks-139",
+        "name": "Osage County, KS",
+        "value": 4.7
+    },
+    {
+        "code": "us-ks-141",
+        "name": "Osborne County, KS",
+        "value": 2.5
+    },
+    {
+        "code": "us-ks-143",
+        "name": "Ottawa County, KS",
+        "value": 2.9
+    },
+    {
+        "code": "us-ks-145",
+        "name": "Pawnee County, KS",
+        "value": 3
+    },
+    {
+        "code": "us-ks-147",
+        "name": "Phillips County, KS",
+        "value": 2.8
+    },
+    {
+        "code": "us-ks-149",
+        "name": "Pottawatomie County, KS",
+        "value": 3.2
+    },
+    {
+        "code": "us-ks-151",
+        "name": "Pratt County, KS",
+        "value": 2.8
+    },
+    {
+        "code": "us-ks-153",
+        "name": "Rawlins County, KS",
+        "value": 2.2
+    },
+    {
+        "code": "us-ks-155",
+        "name": "Reno County, KS",
+        "value": 4
+    },
+    {
+        "code": "us-ks-157",
+        "name": "Republic County, KS",
+        "value": 2.5
+    },
+    {
+        "code": "us-ks-159",
+        "name": "Rice County, KS",
+        "value": 3.2
+    },
+    {
+        "code": "us-ks-161",
+        "name": "Riley County, KS",
+        "value": 3
+    },
+    {
+        "code": "us-ks-163",
+        "name": "Rooks County, KS",
+        "value": 4
+    },
+    {
+        "code": "us-ks-165",
+        "name": "Rush County, KS",
+        "value": 2.7
+    },
+    {
+        "code": "us-ks-167",
+        "name": "Russell County, KS",
+        "value": 3.1
+    },
+    {
+        "code": "us-ks-169",
+        "name": "Saline County, KS",
+        "value": 3.2
+    },
+    {
+        "code": "us-ks-171",
+        "name": "Scott County, KS",
+        "value": 2
+    },
+    {
+        "code": "us-ks-173",
+        "name": "Sedgwick County, KS",
+        "value": 4.2
+    },
+    {
+        "code": "us-ks-175",
+        "name": "Seward County, KS",
+        "value": 3.2
+    },
+    {
+        "code": "us-ks-177",
+        "name": "Shawnee County, KS",
+        "value": 3.8
+    },
+    {
+        "code": "us-ks-179",
+        "name": "Sheridan County, KS",
+        "value": 2.3
+    },
+    {
+        "code": "us-ks-181",
+        "name": "Sherman County, KS",
+        "value": 2.8
+    },
+    {
+        "code": "us-ks-183",
+        "name": "Smith County, KS",
+        "value": 2.9
+    },
+    {
+        "code": "us-ks-185",
+        "name": "Stafford County, KS",
+        "value": 2.4
+    },
+    {
+        "code": "us-ks-187",
+        "name": "Stanton County, KS",
+        "value": 2.8
+    },
+    {
+        "code": "us-ks-189",
+        "name": "Stevens County, KS",
+        "value": 2.7
+    },
+    {
+        "code": "us-ks-191",
+        "name": "Sumner County, KS",
+        "value": 3.6
+    },
+    {
+        "code": "us-ks-193",
+        "name": "Thomas County, KS",
+        "value": 2.6
+    },
+    {
+        "code": "us-ks-195",
+        "name": "Trego County, KS",
+        "value": 3.7
+    },
+    {
+        "code": "us-ks-197",
+        "name": "Wabaunsee County, KS",
+        "value": 4.2
+    },
+    {
+        "code": "us-ks-199",
+        "name": "Wallace County, KS",
+        "value": 2.6
+    },
+    {
+        "code": "us-ks-201",
+        "name": "Washington County, KS",
+        "value": 3
+    },
+    {
+        "code": "us-ks-203",
+        "name": "Wichita County, KS",
+        "value": 2.5
+    },
+    {
+        "code": "us-ks-205",
+        "name": "Wilson County, KS",
+        "value": 4.8
+    },
+    {
+        "code": "us-ks-207",
+        "name": "Woodson County, KS",
+        "value": 4.2
+    },
+    {
+        "code": "us-ks-209",
+        "name": "Wyandotte County, KS",
+        "value": 6.2
+    }]
